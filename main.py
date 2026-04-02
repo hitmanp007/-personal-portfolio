a1aeaa341d478927e9f7bc2a93e93250e934b585
pranav sahu <pranavsahu688@gmail.com>
2026-04-02 13:28:15 +0530

    updated portfolio UI

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask , render_template
+
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
+
+
+
+app.run(debug=True)
